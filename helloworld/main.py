91d672bcdd6cc42b4b2ca9825a1e6a0b88273b52
PROBE <86278730+fellowking00@users.noreply.github.com>
2023-04-09 03:06:38 +0530

    Update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -4,12 +4,13 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 from flask import Flask, request
 
-
-public_key = request.args.get('public_key', '')
+@app.route('/encrypt')
+def encryptt():
+key = request.args.get('key', '')
 card_no = request.args.get('card_no', '')
 
 # Create RSA key object from public key
-rsa_key = RSA.importKey(public_key)
+rsa_key = RSA.importKey(key)
 
 # Create cipher object using PKCS1 v1.5 padding scheme
 cipher = PKCS1_v1_5.new(rsa_key)
@@ -21,3 +22,7 @@ encrypted_card_no = cipher.encrypt(card_no.encode('utf-8'))
 encoded_card_no = base64.b64encode(encrypted_card_no)
 
 print(encoded_card_no.decode())
+
+if __name__ == '__main__':
+
+    app.run()
