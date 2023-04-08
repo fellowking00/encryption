6ed895d2d5f12f5ecf86695f2cf14bdf736ca257
PROBE <86278730+fellowking00@users.noreply.github.com>
2023-04-09 02:53:32 +0530

    Update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,8 +1,23 @@
 
-from flask import Flask
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from flask import Flask, request
 
-app = Flask(__name__)
 
-@app.route("/")
-def home_view():
-    return "Hello world"
+public_key = request.args.get('public_key', '')
+card_no = request.args.get('card_no', '')
+
+# Create RSA key object from public key
+rsa_key = RSA.importKey(public_key)
+
+# Create cipher object using PKCS1 v1.5 padding scheme
+cipher = PKCS1_v1_5.new(rsa_key)
+
+# Encrypt the card number using cipher
+encrypted_card_no = cipher.encrypt(card_no.encode('utf-8'))
+
+# Encode the encrypted message in base64
+encoded_card_no = base64.b64encode(encrypted_card_no)
+
+print(encoded_card_no.decode())
