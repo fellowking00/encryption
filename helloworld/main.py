33e47573758ad7238838a7e841cf5d3ca6ba1673
PROBE <86278730+fellowking00@users.noreply.github.com>
2023-04-09 05:40:51 +0530

    Update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,42 +1,38 @@
 
 import base64
+import os
 
 from Crypto.PublicKey import RSA
-
 from Crypto.Cipher import PKCS1_v1_5
-
 from flask import Flask, request
 
 app = Flask(__name__)
+app.config['ENV'] = 'production' # Set Flask environment to production
+app.debug = False # Disable debug mode
 
 @app.route('/encrypt')
-
 def encryptt():
-
     key = request.args.get('key', '')
-
     card_no = request.args.get('card_no', '')
+   
+    if not key or not card_no:
+        return "Key and card_no query parameters are required.", 400
 
-    # Create RSA key object from public key
-
-    rsa_key = RSA.importKey(key)
-
-    # Create cipher object using PKCS1 v1.5 padding scheme
+    try:
+        rsa_key = RSA.import_key(key)
+    except ValueError:
+        return "Invalid key format. Please provide a valid RSA public key format.", 400
 
     cipher = PKCS1_v1_5.new(rsa_key)
+    
+    try:
+        encrypted_card_no = cipher.encrypt(card_no.encode('utf-8'))
+    except ValueError:
+        return "Invalid card number. Please provide a valid credit card number.", 400
 
-    # Encrypt the card number using cipher
+    encoded_card_no = base64.b64encode(encrypted_card_no).decode('utf-8')
 
-    encrypted_card_no = cipher.encrypt(card_no.encode('utf-8'))
-
-    # Encode the encrypted message in base64
-
-    encoded_card_no = base64.b64encode(encrypted_card_no)
-
-    print(encoded_card_no.decode())
-
-    return encoded_card_no.decode()
+    return encoded_card_no, 200
 
 if __name__ == '__main__':
-
     app.run()
